6c735c081650dc0b1371ef91920d78fa1314ed89
Valeria Kryshchanovich <lera_krishanovich@mail.ru>
2022-02-03 14:30:27 +0300

    solve fizzBuzz

--- 02-fizzbuzz/script.js
+++ 02-fizzbuzz/script.js
@@ -7,11 +7,32 @@
  * • кратные трём — на fizz;
  * • кратные пяти — на buzz;
  * • кратные и трём, и пяти одновременно — на fizzbuzz.
+ *
+ * 
+ *  fizzBuzz(15) -> 1 2 fizz 4 buzz fizz buzz ... fizzbuzz
  * 
 */
 
+function checkFizzBuzz(num) {
+    if (num % 3 === 0 && num % 5 === 0) {
+         return 'fizzbuzz'
+    } else if (num % 5 === 0){
+        return 'buzz'
+    } else if (num % 3 === 0){
+        return 'fizz'
+    }
+     return num
+}
+
 function fizzBuzz(num) {
-    // Напишите код здесь
+    /*
+    1. Создать последовательность чисел от 1 до num
+    2. Проверить числа от 1 до num при помощи ф-ции checkFizzBuzz
+    */
+   for(let i = 1; i <= num; i++){
+       const value = checkFizzBuzz(i);
+       console.log(value)
+   }
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
